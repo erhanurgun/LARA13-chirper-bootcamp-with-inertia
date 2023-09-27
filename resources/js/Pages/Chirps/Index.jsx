7fc69cda9c92e2b61c58262a9e0c468862e088db
erhanurgun <erhan.rgn04@gmail.com>
2023-09-27 18:27:48 +0300

    Veri listeleme ile ilgili ayarlamalar yapildi

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import Chirp from '@/Components/Chirp';
 import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import {useForm, Head} from '@inertiajs/react';
 
-export default function Index({auth}) {
+export default function Index({ auth, chirps }) {
     const {data, setData, post, processing, reset, errors} = useForm({
         message: '',
     });
@@ -17,7 +18,6 @@ export default function Index({auth}) {
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="Chirps"/>
-
             <div className="mx-auto p-4 sm:p-6 lg:p-8" style={{maxWidth: '1140px'}}>
                 <form onSubmit={submit}>
                     <textarea
@@ -29,6 +29,11 @@ export default function Index({auth}) {
                     <InputError message={errors.message} className="mt-2"/>
                     <PrimaryButton className="mt-4" disabled={processing}>Chirp</PrimaryButton>
                 </form>
+                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                    {chirps.map(chirp =>
+                        <Chirp key={chirp.id} chirp={chirp} />
+                    )}
+                </div>
             </div>
         </AuthenticatedLayout>
     );
