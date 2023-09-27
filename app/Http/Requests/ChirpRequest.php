2906b0a7335e58dce9ed6f293979151615310a3d
erhanurgun <erhan.rgn04@gmail.com>
2023-09-27 18:11:34 +0300

    Veri ekleme ile ilgili ayarlamalar yapildi

---
+++ app/Http/Requests/ChirpRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChirpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'message' => 'required|string|max:255',
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'message.required' => 'A chirp is required.',
+            'message.string' => 'A chirp must be a string.',
+            'message.max' => 'A chirp cannot be longer than 255 characters.',
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'message' => 'chirp',
+        ];
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Erho DEV',
+            'email' => 'me@erho.dev',
+            'password' => bcrypt('E4i@wbSve7k$#Nk1Vc'),
+        ]);
+
+        \App\Models\User::factory()->count(10)->create();
+    }
+}
